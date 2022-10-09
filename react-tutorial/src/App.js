f0e9bc0833c7d982504d11155d7df20520bf2cd6
aulanand <atulanandmishra@gamil.com>
2022-10-09 13:30:03 +0530

    Basic State

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/NavBar';
+import TextArea from './component/TextArea';
 
 
 function App() {
   return (
    <>
     <NavBar navBarTitle='atquil'/>
+    <div className="container mx-2">
+      <TextArea></TextArea>
+    </div>    
    </>
   );
 }
---
+++ react-tutorial/src/component/TextArea.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function TextArea() {
+
+    const [text,setText] = useState('');
+
+    const textToUpperCase = () => {
+        let upperCaseValue = text.toUpperCase();
+        setText(upperCaseValue);
+    }
+    const textOnChange = (event)=>{
+        setText(event.target.value);
+    }
+  return (
+    <>
+       <h1>State Change</h1>
+        <div className="mb-3">
+            <label for="exampleFormControlTextarea1" className="form-label">Your Story</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"  onChange={textOnChange}></textarea>
+        </div>
+        <p> Output text: {text}</p>
+        <button type="button" className="btn btn-primary" onClick={textToUpperCase}>To Upper Case</button>
+    </>
+  )
+}
